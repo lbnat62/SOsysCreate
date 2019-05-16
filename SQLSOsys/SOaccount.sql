2a0d2902faac8d1c1d9beeadf7fbdaeb5368a1d4
lbnat62 <44816579+lbnat62@users.noreply.github.com>
2019-05-16 13:59:29 -0700

    tested create account

---
+++ SQLSOsys/SOaccount.sql
@@ -0,0 +1,35 @@
+/* create new user account */
+set echo on
+
+spool t:account.txt
+
+prompt ****** CREATE AN ACCOUNT ******
+prompt 
+select 'Today''s Date: ' ||to_char(sysdate, 'Month dd, yyyy') from dual;
+
+prompt
+prompt
+accept v_FName prompt 'Please enter your first name: '
+prompt
+accept v_LName prompt 'Please enter your last name: '
+prompt
+accept v_Address prompt 'Shipping Address: '
+accept v_City prompt 'City: '
+accept v_State prompt 'State: '
+accept v_ZIP prompt 'Zip Code: '
+accept v_Phone prompt 'Phone Number: '
+prompt
+prompt
+insert into Customers values (
+	(CNum_seq.nextval),
+	'&v_FName',
+	'&v_LName',
+	'&v_Address',
+	'&v_City',
+	'&v_State',
+	'&v_ZIP',
+	'&v_Phone');
+
+prompt *** YOUR ACCOUNT HAS BEEN CREATED ***
+select 'Your Account Number is: '||CNum_seq.currval from dual;
+select * from Customers where CustomerID=(select max(CustomerID) from Customers);
